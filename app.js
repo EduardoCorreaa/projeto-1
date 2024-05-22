e07eefbb0608dcc9114fd63397d7250ef9d34c69
EduardoCorreaa <eduardo.correahc@gmail.com>
2024-05-22 20:00:48 -0300

    Projeto 1

---
+++ app.js
@@ -0,0 +1,30 @@
+let titulo = document.querySelector ('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+function verificarConsole() {
+    console.log('O botão foi Clicado')
+}
+
+function verificarAlert() {
+    console.log('Eu amo JS')
+}
+
+function verificarPrompt() {
+    console.log('Estive em Recife e lembrei de você')
+}
+
+function verificarSoma() {
+console.log ('Te amo, Gabriele')
+}
+
+function exibirOla() {
+    console.log("Olá, mundo!");
+  }
+  
+  exibirOla();
+
+  function exibirOlaNome(nome) {
+    console.log(`Olá, ${nome}!`);
+  }
+  
+  exibirOlaNome("Eduardo");
